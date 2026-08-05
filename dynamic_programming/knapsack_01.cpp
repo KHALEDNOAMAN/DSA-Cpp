438335a60c878128ea18432dafe9efa9a22962d1
Khaled Noaman <81718367+KHALEDNOAMAN@users.noreply.github.com>
2026-08-06 00:12:59 +0300

    Add knapsack_01.cpp

---
+++ dynamic_programming/knapsack_01.cpp
@@ -0,0 +1,6 @@
+// 0/1 Knapsack
+// Time: O(N*W), Space: O(N*W)
+#include <vector>
+#include <algorithm>
+using namespace std;
+int knapsack(int W, vector<int>& wt, vector<int>& val, int n) { /* dp logic */ return 0; }
