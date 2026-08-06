f172706304e9b542e0f336daab1242922ab1156d
Khaled Noaman <81718367+KHALEDNOAMAN@users.noreply.github.com>
2026-08-06 19:10:21 +0300

    Add file

---
+++ strings/rabin_karp.cpp
@@ -0,0 +1,8 @@
+// Rabin Karp Algorithm in C++
+#include <iostream>
+#include <string>
+using namespace std;
+
+void rabinKarp(string pattern, string text) {
+    cout << "Rabin Karp Algorithm placeholder" << endl;
+}
