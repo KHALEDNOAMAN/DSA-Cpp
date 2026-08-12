cb3e7023f5e5e4d3f309b1dfc8ce2ea85410a27b
Khaled Noaman <81718367+KHALEDNOAMAN@users.noreply.github.com>
2026-08-12 19:01:03 +0300

    Add missing number using XOR

---
+++ bit_manipulation/missing_number.cpp
@@ -0,0 +1,7 @@
+#include <vector>
+using namespace std;
+int missingNumber(vector<int>& nums) {
+    int res = nums.size();
+    for(int i=0; i<nums.size(); i++) res ^= i ^ nums[i];
+    return res;
+}
